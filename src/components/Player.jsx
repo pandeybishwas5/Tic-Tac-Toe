dbd1ae1680206a99abd66d1fa7bb12340275616a
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-03-29 01:24:43 +1100

    Add rematch function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { React, useState } from 'react';
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
     const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick(){ //if not editing, turn on edit mode. If already in edit mode, turn it off.
         setIsEditing( (editing)  => !editing);
+        if (isEditing){
+            onChangeName(symbol, playerName);
+        }
+        
     }
 
     function handleChange(event) {
