927be62b885b66e835c623c26cd8e37ba1d5de94
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-03-28 23:37:48 +1100

    Add active player functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import Player from './components/Player.jsx'
+import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
+import { useState } from 'react';
+
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((playerSymbol) => playerSymbol === 'X' ? 'O' : 'X');
+  }
 
   return (
     <main>
-    <div id="game-container">
-      <ol id="players">  
-        <Player initialName="Player 1" symbol="X" />
-        <Player initialName="Player 2" symbol="X" />
-      </ol>
-      <GameBoard />
-    </div>
-    LOG
-  </main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+      </div>
+      LOG
+    </main>
   );
-  
 }
 
-export default App
+export default App;
